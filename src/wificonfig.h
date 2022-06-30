bbd92116e86c63c73b0e4afa464111e25aaf5463
Atilli <xitxat@outlook.com>
2022-06-30 15:45:58 +0200

    creds

---
+++ src/wificonfig.h
@@ -0,0 +1,8 @@
+#ifndef WIFICONFIG_h
+#define WIFICONFIG_h
+
+
+
+const char* ssid = "MOVISTAR_90A8";
+const char* passwd = "kXi73VCP3inddNJegngg";
+const char* bingMapsKey = "AnUzCCHCdx4qcjB8M2gMErKqpULbDvknWlABjyVb50Ul26IeOb15HyIc00bkmZhf";
