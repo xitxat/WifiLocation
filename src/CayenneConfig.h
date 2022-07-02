17a434aa3174dace2ed84848a21487e040faae15
Atilli <xitxat@outlook.com>
2022-07-02 18:44:07 +0200

    working

---
+++ src/CayenneConfig.h
@@ -0,0 +1,11 @@
+#ifndef CAYENNECONFIG_H
+#define CAYENNECONFIG_H
+
+
+// Cayenne authentication info. This should be obtained from the Cayenne Dashboard.
+char username[] = "4c8b8bd0-7d42-11ec-8da3-474359af83d7";
+char password[] = "caa9cff4a68e7323b043b5d165f94b62bd4dd670";
+char clientID[] = "28605410-f9f8-11ec-8df2-dd50487e509b";
+
+
+#endif
