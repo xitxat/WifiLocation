93a6f2eb9918dbab96ab8c646434f794fb0affc5
Atilli <xitxat@outlook.com>
2022-07-02 18:46:33 +0200

    config

--- src/main.cpp
+++ src/main.cpp
@@ -9,12 +9,13 @@
 #include <Wire.h> //  I2C
 #include <CayenneMQTTESP8266.h>
 #include <SimpleTimer.h>
+#include "CayenneConfig.h"
 
 #define CAYENNE_PRINT Serial
-// Cayenne authentication info. This should be obtained from the Cayenne Dashboard.
-char username[] = "4c8b8bd0-7d42-11ec-8da3-474359af83d7";
-char password[] = "caa9cff4a68e7323b043b5d165f94b62bd4dd670";
-char clientID[] = "28605410-f9f8-11ec-8df2-dd50487e509b";
+// // Cayenne authentication info. This should be obtained from the Cayenne Dashboard.
+// char username[] = "4c8b8bd0-7d42-11ec-8da3-474359af83d7";
+// char password[] = "caa9cff4a68e7323b043b5d165f94b62bd4dd670";
+// char clientID[] = "28605410-f9f8-11ec-8df2-dd50487e509b";
 
 // Timer
 SimpleTimer timer;
